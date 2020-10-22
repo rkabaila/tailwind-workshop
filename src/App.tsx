ab14251e5e8e61a9e7b68fbd4bbf52ebf7c70fc6
Rimas Kabaila <rimas.kabaila@gmail.com>
2020-10-22 11:26:19 +0300

    added 2 tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const App = () => {
-  return <div className="m-10">test</div>;
+  return (
+    <div className="flex flex-col items-center mx-8">
+      <h1>Couriers page</h1>
+      <h2>Couriers list</h2>
+    </div>
+  );
 };
